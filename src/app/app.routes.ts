d1bdadf1631060ecb93ab837ed55e43ee4eabca8
Jinu P Rajan <jinoyrajan832@gmail.com>
2026-06-09 07:14:08 +0530

    Setup Angular enterprise structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home/home').then(m => m.Home)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard').then(m => m.Dashboard)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login/login').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register/register').then(m => m.Register)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./layout/navbar/navbar";
+import { Login } from "./features/auth/login/login/login";
+import { Register } from "./features/auth/register/register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
